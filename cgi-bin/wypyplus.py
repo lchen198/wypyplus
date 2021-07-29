9c23f0e58d54b937f7e261d6d9c4384da85ec109
LC <lchen198@gmail.com>
2021-07-29 15:52:05 -0700

    Create a search link using ?(words). It is useful to have links such as ?TODO and ?DEADLINE.

--- cgi-bin/wypyplus.py
+++ cgi-bin/wypyplus.py
@@ -14,11 +14,11 @@ f,i=cgi.FormContent(),'put type';y=f.get('p',[''])[0];y=dt.now().strftime("%b%d"
 se='<form><input type="text"placeholder="Search.. "name="p"><input type="hidden" name="q" value="f"><button type="submit">Search</button></form>'
 fs=lambda s:re.sub(pre_h,remove_leading_space,reduce(lambda s,r:re.sub('(?m)'+r[0],r[1],s),(('\r',''),
 ('^@INCLUDE=(\w+)$',lambda m: x('w/'+m.group(1)) and open('w/'+m.group(1)).read() or ''),
+('\?(\w+)',lambda m: h+w+m.group(1)+'&amp;q=f>'+m.group(0)+'</a>'),
 ('(^|[^=/\-_A-Za-z0-9?])([A-Z][a-z]+([A-Z0-9][a-z0-9]*){1,})',
  lambda m:(m.group(1)+'%s%s')%((m.group(2),h+w+m.group(2)+'&amp;q=e>?</a>' if edit else ''),('',h+w+m.group(2)+'>%s</a>'%m.group(2)))[x('w/'+m.group(2))]),
 ('^\{\{$','\n<ul>'),('^\*(.*)$','<li>\g<1></li>'),('^}}$','</ul>'),('^---$','<hr>'),
-(pre,'<pre><code>\g<1></code></pre>'),
-('^# (.*)$',hl1),('^## (.*)$', hl2),('^### (.*)$',hl3),('\*\*(.*)\*\*','<b>\g<1></b>'),
+(pre,'<pre><code>\g<1></code></pre>'),('^# (.*)$',hl1),('^## (.*)$', hl2),('^### (.*)$',hl3),('\*\*(.*)\*\*','<b>\g<1></b>'),
 ('\!'+link,'<img src="\g<2>" alt="\g<1>">'),('(^|[^!])'+link,"\g<1>"+h+'"\g<3>">\g<2></a>'),
 ('(^|[^"])(http[s]?:[^<>"\s]+)',
  lambda m: ('<iframe width="560" height="315" src="https://www.youtube.com/embed/%s" \
@@ -34,7 +34,7 @@ do=lambda m,n:{
         w,n,fs(n),i,n, dt.now().strftime("%m/%d/%Y %H:%M"), (dt.now()+td(minutes=30)).strftime("%H:%M"), q(load_tpl(n)),t,i),
     'find':('<h1>Links: %s</h1>'%fs(n))+fs('\n---\n'.join(
         sorted(filter(lambda x: not x.endswith(':\n\n'),[d if n == "All" or n in d else d+':\n\n'+'\n\n'.join(
-            [line for line in load(d).splitlines() if n in line]) for d in os.listdir('w/')]))))}.get(m)
+            [line for line in load(d).splitlines() if n in line and '?'+n not in line]) for d in os.listdir('w/')]))))}.get(m)
 main=lambda f=f:`(os.getenv("REQUEST_METHOD")!="POST") or not edit or ('t' in f or (os.remove('w/'+y) and False))\
     and open('w/'+y,'w').write(f['t'][0])`+`sys.stdout.write("Content-type: text/html; charset=utf-8\r\n\r\n"\
         '<head><meta content="width=device-width, initial-scale=1" name="viewport"><link rel="stylesheet" href="../sakura.css">\
