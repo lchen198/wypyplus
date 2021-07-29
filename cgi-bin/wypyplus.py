d40ffec84cbc0699586b0eb99b807d1a8f4cba8d
LC <lchen198@gmail.com>
2021-07-28 17:03:12 -0700

    Add an example hard copy and presentation instructions

--- cgi-bin/wypyplus.py
+++ cgi-bin/wypyplus.py
@@ -27,8 +27,8 @@ frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media
  else (m.group(1)+h+m.group(2)+">"+m.group(2)+"</a>")),
 ('\n\n','\n<p>')),q(s)),0,re.MULTILINE)
 do=lambda m,n:{
-    'get':'<h1>%s%s%s>%s</a>'%(h,w,home,home) + ':%s%s%s&amp;q=f>%s</a>'%(h,w,n,n) +
-    (('%s%s%s&amp;q=e>%s</a>'%(h,w,n,edit)) if edit else '') +
+    'get':'<h1>%s%s%s>%s</a>'%(h,w,home,home) +
+    ((':%s%s%s&amp;q=f>%s</a>%s%s%s&amp;q=e>%s</a>'%(h,w,n,n,h,w,n,edit)) if edit else '') +
     '</h1>%s<p>%s'%(se if edit else '',fs(load_g()+re.sub(pre, insert_leading_space, load_tpl(n))) or n),
     'edit':'<form name="e" action=%s%s method=POST><h1>%s <in%s=hidden name=p value=%s></h1>Opened at: %s AutoSave at: %s<textarea name=t id=ta rows=24>%s%s<in%s=submit>'%(
         w,n,fs(n),i,n, dt.now().strftime("%m/%d/%Y %H:%M"), (dt.now()+td(minutes=30)).strftime("%H:%M"), q(load_tpl(n)),t,i),
