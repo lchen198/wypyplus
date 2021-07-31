4f6f711a712c9a2eeb247ca00725ec7d7f680269
LC <lchen198@gmail.com>
2021-07-31 11:40:04 -0700

    Support RPN column formula.

--- cgi-bin/rpn.py
+++ cgi-bin/rpn.py
@@ -1,9 +1,58 @@
 import operator as op;import math,re
-unary_op=lambda t,s: getattr(math,t)(s.pop());norm_op=lambda t,s: t(s.pop(-2),s.pop());rev_op=lambda t,s: t(s.pop(),s.pop())
-unary=['sqrt','fabs','sin','cos','tan'];norm={'+':op.add,'-':op.sub,'*':op.mul,'/':op.div,'^':pow};rev={'log':math.log};
-ops=lambda t,s: unary_op(t,s) if t in unary else norm_op(norm[t],s) if t in norm else rev_op(rev[t],s) if t in rev else 'err'
-def rpn(v,env=[]):
+unary_op=lambda t,s: t(s.pop())
+norm_op=lambda t,s: t(s.pop(-2),s.pop())
+rev_op=lambda t,s: t(s.pop(),s.pop())
+unary={'sqrt':math.sqrt, 'fabs':math.fabs, 'sin':math.sin, 'cos':math.cos,'tan':math.tan, 'sum':sum, 'avg': lambda v: sum(v)/len(v)}
+norm={'+':op.add,'-':op.sub,'*':op.mul,'/':op.div,'^':pow}
+rev={'log':math.log};
+ops=lambda t,s: unary_op(unary[t],s) if t in unary else norm_op(norm[t],s) if t in norm else rev_op(rev[t],s) if t in rev else 'err'
+
+def ref(t, col, env, table):
+    if t.startswith('$'):
+        rg = re.match('\$(\d+)\.\.\$(\d+)',t)
+        if rg:
+            start, end = int(rg.group(1)), int(rg.group(2))
+            return [float(v) for v in env[start:end+1]]
+        else:
+            if int(t[1:]) < len(env):
+                return float(env[int(t[1:])] or 0 )
+    elif t.startswith('@'):
+        rg = re.match('\@(\d+)\.\.\@(\d+)',t)
+        if rg:
+            start, end = int(rg.group(1)), int(rg.group(2))
+            return [float(r[col]) for i,r in enumerate(table) if start <= i <= end]
+        else:
+            row = int(t[1:])
+            if (row < len(table)):
+                return float(table[row][col] or 0 )
+    return 'invalid reference (%s, %s)' % (t, col)
+    
+def rpn(v,col=0, env=[],table=[]):    
+    s=[]
     try:
-        s=[];[s.append(float(t)) if set(t).issubset(set("0123456789.-")) else s.append(float(env[int(t[1:])] or 0 )) if t.startswith('$') else s.append(ops(t,s)) for t in v.split()];return str(s[0]) if len(s)==1 else str(s)
-    except: return 'err'
-def rpn_row(m):env=[];[env.append(str(v) if not v.startswith('=') else rpn(v[1:],env)) for v in map(str.strip,re.split('\|{1,2}',m)[1:])];return '<tr><td>'+'</td><td>'.join(env)+'</td></tr>'
+        for t in v.split():
+            if set(t).issubset(set("0123456789.-")):
+                s.append(float(t)) 
+            elif t.startswith('$') or t.startswith('@'):
+                s.append(ref(t,col,env,table))
+            else:
+                s.append(ops(t,s))
+    except Exception as e:
+        return 'err: %s %s'%(str(s),str(e))
+    return str(s[0]) if len(s)==1 else str(s)
+
+def rpn_row(row, table_env):
+    env=[]
+    for col, v in enumerate(map(str.strip,re.split('\|{1,2}',row)[1:])):
+        env.append(str(v) if not v.startswith('=')
+                       else rpn(v[1:],col,env,table_env))
+    return env
+
+def rpn_table(m):
+    table_env=[]
+    table = []
+    for row in re.split('\|-',m.group(2)):
+        row_env = rpn_row(row, table_env)
+        table.append('<tr><td>'+'</td><td>'.join(row_env)+'</td></tr>')
+        table_env.append(row_env)
+    return '<table>'+'\n'.join(table)+'</table>'
--- cgi-bin/wypyplus.py
+++ cgi-bin/wypyplus.py
@@ -16,8 +16,8 @@ y=f.get('p',[''])[0];y=dt.now().strftime("%b%d") if y=='Today' else (home,y)[y.i
 se='<form><input type="text"placeholder="Search.. "name="p"><input type="hidden" name="q" value="f"><button type="submit">Search</button></form>'
 fs=lambda s:re.sub(pre_h,remove_leading_space,reduce(lambda s,r:re.sub('(?m)'+r[0],r[1],s),(('\r',''),
 ('^INCLUDE\((\w+)\)$',lambda m: '\n'.join((lambda l: sum(map(flatten,l),[]) if isinstance(l,list) else [l])(load_rec(m.group(1))))),
-('(\{\|\n)(.*[^\}]+)(\|\})',lambda m:'<table>'+'\n'.join([rpn.rpn_row(line) for line in re.split('\|-',m.group(2))])+'</table>'),
-('RPN\((.*?)\)', lambda m: rpn.rpn(m.group(1))),('(^|[^=/\-_A-Za-z0-9?])@(\w+)',lambda m: h+w+m.group(2)+'&amp;q=f>@'+m.group(2)+'</a>'),
+('(\{\|\n)(.*[^\}]+)(\|\})',rpn.rpn_table),
+('RPN\((.*?)\)', lambda m: rpn.rpn(m.group(1))),('(^|[^=/\-_A-Za-z0-9?])@(\w\w+)',lambda m: h+w+m.group(2)+'&amp;q=f>@'+m.group(2)+'</a>'),
 ('(^|[^=/\-_A-Za-z0-9?])([A-Z][a-z]+([A-Z0-9][a-z0-9]*){1,})',
      lambda m:(m.group(1)+'%s%s')%((m.group(2),h+w+m.group(2)+'&amp;q=e>?</a>' if edit else ''),
                                        ('',h+w+m.group(2)+'>%s</a>'%m.group(2)))[x('w/'+m.group(2))]),
@@ -27,8 +27,8 @@ fs=lambda s:re.sub(pre_h,remove_leading_space,reduce(lambda s,r:re.sub('(?m)'+r[
  lambda m: ('<iframe width="560" height="315" src="https://www.youtube.com/embed/%s" frameborder="0" allow="accelerometer; autoplay;\
  clipboard-write; encrypted-media;gyroscope; picture-in-picture" allowfullscreen></iframe>' % m.group(2)[len(yt):]) if m.group(2).startswith(yt)
  else (m.group(1)+h+m.group(2)+">"+m.group(2)+"</a>")),('\n\n','\n<p>')),q(s)),0,re.MULTILINE)
-do=lambda m,n:{'get':lambda:'<h1>%s%s%s>%s</a>'%(h,w,home,home) + ((':%s%s%s&amp;q=f>%s</a>%s%s%s&amp;q=e>%s</a>'%(h,w,n,n,h,w,n,edit)) if edit else '') +
-      '</h1>%s<p>%s'%(se if edit else '',fs(load_g()+re.sub(pre, insert_leading_space, load_tpl(n))) or n),
+do=lambda m,n:{'get':lambda:'<div class="navbar"><h1>%s%s%s>%s</a>'%(h,w,home,home) + ((':%s%s%s&amp;q=f>%s</a>%s%s%s&amp;q=e>%s</a>'%(h,w,n,n,h,w,n,edit)) if edit else '') +
+      '</h1>%s</div><div class="main"><p>%s'%(se if edit else '',fs(load_g()+re.sub(pre, insert_leading_space, load_tpl(n))) or n),
     'edit':lambda:'<form name="e" action=%s%s method=POST><h1>%s <in%s=hidden name=p value=%s></h1>\
 Opened at: %s AutoSave at: %s<textarea name=t id=ta rows=24>%s%s<in%s=submit>'%(
         w,n,fs(n),i,n, dt.now().strftime("%m/%d/%Y %H:%M"), (dt.now()+td(minutes=30)).strftime("%H:%M"), q(load_tpl(n)),t,i),
@@ -38,5 +38,5 @@ Opened at: %s AutoSave at: %s<textarea name=t id=ta rows=24>%s%s<in%s=submit>'%(
 main=lambda f=f:`(os.getenv("REQUEST_METHOD")!="POST") or not edit or ('t' in f or (os.remove('w/'+y) and False))\
     and open('w/'+y,'w').write(f['t'][0])`+`sys.stdout.write("Content-type: text/html; charset=utf-8\r\n\r\n"\
         '<head><meta content="width=device-width, initial-scale=1" name="viewport"></script><link rel="stylesheet" href="../sakura.css">\
-        <script type="text/javascript" src="../ASCIIMathML.js"></script></head><title>%s</title>'%y+
+        <link rel="stylesheet" href="../nav.css"><script type="text/javascript" src="../ASCIIMathML.js"></script></head><title>%s</title>'%y+
 do(({'e':'edit','f':'find'} if edit else {'f':'find'}).get(f.get('q',[None])[0],'get'),y))`;(__name__=="__main__") and main()
