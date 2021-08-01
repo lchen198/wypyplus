b47a44cde9ccf42d71b4f6c8799c0c0ed43e8ab8
LC <lchen198@gmail.com>
2021-08-01 11:22:21 -0700

    Support string and add printing functions.

--- cgi-bin/rpn.py
+++ cgi-bin/rpn.py
@@ -1,22 +1,28 @@
-import operator as op;import math,re
+import operator as op;import math,re, random
 unary_op=lambda t,s: t(s.pop())
 norm_op=lambda t,s: t(s.pop(-2),s.pop())
 rev_op=lambda t,s: t(s.pop(),s.pop())
 unary={'sqrt':math.sqrt, 'fabs':math.fabs, 'sin':math.sin, 'cos':math.cos,'tan':math.tan,
            'sum':sum, 'avg': lambda v: sum(v)/len(v)}
-def swap(s) : a=s.pop(); b=s.pop(); s.append(a); s.append(b)
-def rot(s) :a=s.pop(); b=s.pop(); c=s.pop(); s.append(b), s.append(a), s.append(c)
-stack={'dup': lambda s: s.append(s[-1]), 'swap': swap, 'drop': lambda s:s.pop(),
-  'over': lambda s: s.append(s[-2]), 'rot': rot}
+def swap(s, out) : a=s.pop(); b=s.pop(); s.append(a); s.append(b)
+def rot(s, out) :a=s.pop(); b=s.pop(); c=s.pop(); s.append(b), s.append(a), s.append(c)
+stack={
+    'dup': lambda s,out: s.append(s[-1]),
+    'swap': swap,
+    'drop': lambda s, out:s.pop(),
+    'over': lambda s, out: s.append(s[-2]),
+    'rot': rot,
+    '.' : lambda s, out: out.append(str(s.pop())),
+    'cr' : lambda s, out: out.append('\n')
+    }
 
-norm={'+':op.add,'-':op.sub,'*':op.mul,'/':op.div,'^':pow}
+norm={'+':op.add,'-':op.sub,'*':op.mul,'/':op.div,'^':pow, 'randint':random.randint}
 rev={'log':math.log};
 ops=lambda t,s: unary_op(unary[t],s) if t in unary \
   else norm_op(norm[t],s) if t in norm \
   else rev_op(rev[t],s) if t in rev \
   else 'undefined function:' + t
 user_ops = {}
-log = open('./log','a')
 def ref(t, col, env, table):
     if t.startswith('$'):
         rg = re.match('\$(\d+)\.\.\$(\d+)',t)
@@ -38,32 +44,66 @@ def ref(t, col, env, table):
     return 'invalid reference (%s, %s)' % (t, col)
 
 def rpn(v,col=0, env=[],table=[]):
-    global user_ops,log
+    global user_ops
     s=[]
+    output=[]
     if v.startswith(':'):
         user_fn = v.strip(';').split()
         user_ops[user_fn[1]] = user_fn[2:]
         return ''
-    words=v.split()
+    def tokenize(v):
+        STR, NON_STR, SP = 1, 2, 3
+        state = SP
+        t = []
+        toks = []
+        for i, c in enumerate(v):
+            if state == SP:
+                if c in ['"',"'"]:
+                    state = STR
+                elif c == " ":
+                    continue
+                else:
+                    state = NON_STR
+            if state == STR:
+              t.append(c)
+              if len(t) > 1 and c == t[0]:
+                  toks.append(''.join(t))
+                  t = []
+                  state = SP
+            if state == NON_STR:
+                if c == ' ':
+                    if t:
+                        toks.append(''.join(t))
+                        t = []
+                        state = SP
+                else:
+                    t.append(c)
+        if t:
+            toks.append(''.join(t))
+        return toks
+    words=tokenize(v)
     try:
         while words:
             word = words[0]
             words = words[1:]
-            log.write(word + str(s)+'\n')
-            if word and set(word).issubset(set("0123456789.-")):
+            if word !='.' and word != '-' and set(word).issubset(set("0123456789.-")):
                 s.append(float(word))
+            elif word.startswith('"') or word.startswith("'"):
+                s.append(word.strip('"\''))
             elif word in user_ops:
                 words = user_ops[word] + words
             elif word.startswith('$') or word.startswith('@'):
                 s.append(ref(word,col,env,table))
             elif word in stack:
-                stack[word](s)
+                stack[word](s,output)
             else:
                 s.append(ops(word,s))
-            log.write('after'+word + str(s)+" user fun" + str(user_ops)+'\n')
     except Exception as e:
-        return 'err: %s %s %s'%(str(s),str(e), str(s))
-    return str(s[0]) if len(s)==1 else str(s)
+        return 'err: %s %s %s'%(str(word),str(e), str(s))
+    if output:
+        return ''.join([str(o) for o in output])
+    else:
+        return str(s[0]) if len(s)==1 else str(s)
 
 def rpn_row(row, table_env):
     env=[]
